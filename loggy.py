a67509d39175ba061ae877bc9a250a4a46bfef7e
Ronald Kaiser <raios.catodicos@gmail.com>
2025-08-08 18:44:30 -0300

    add index in memory

--- loggy.py
+++ loggy.py
@@ -5,25 +5,87 @@
 # Date: 2025-08-08
 
 
+# Problem 1: search is slow, have to go through the whole file to find last key/value.
+# Solution: build index in memory pointing to byte position of value.
+
+# Problem 2: the client looses it's index once the command is run.
+# Solution: have an index loaded in a instance always available.
+
+
+import os
 import sys
 
 
 DATABASE_FILENAME = "/tmp/db.loggy"
+END_RECORD = "\n"
+KEY_VALUE_SEPARATOR = ","
+ENCODING = "utf-8"
+DEBUG = True
+
+
 index = {}
 
 
 def set(key, value):
-	with open(DATABASE_FILENAME, 'a+') as file:
-		file.write(f"{key},{value}\n")
+	if DEBUG:
+		print("Inside set", key, value)
+
+	key_b = key.encode(ENCODING)
+	separator_b = KEY_VALUE_SEPARATOR.encode(ENCODING)
+	value_b = value.encode(ENCODING)
+	end_b = END_RECORD.encode(ENCODING)
+	content = key_b + separator_b + value_b + end_b
+	END = os.path.getsize(DATABASE_FILENAME)
+	value_start = END + len(key_b) + len(separator_b)
+	index[key] = (value_start, len(value_b))
+
+	with open(DATABASE_FILENAME, 'ab+') as file:
+		file.seek(END, 0)
+		file.write(content)
+
+
+def build_index():
+	if DEBUG:
+		print("Bulding index...")
+
+	with open(DATABASE_FILENAME, 'rb') as file:
+		key = ""
+		current = ""
+		start, end = 0, 0
+		while (c:= file.read(1)):
+			c = c.decode(ENCODING)
+			if c == KEY_VALUE_SEPARATOR:
+				key = current
+				current = ""
+				start = file.tell()
+			elif c == END_RECORD:
+				current = ""
+				end = file.tell()
+				index[key] = (start, end - start - 1)
+				start, end = end, end
+			else:
+				current += c
 
 
 def get(key):
-	with open(DATABASE_FILENAME, 'r') as file:
-		value = None
-		for line in file.readlines():
-			k, *v = line.split(",")
-			if k == key:
-				value = v[0][:-1] # remove \n
+	if DEBUG:
+		print("Inside get", key)
+
+	if not index:
+		build_index()
+
+	try:
+		offset, size = index[key]
+	except KeyError:
+		return None
+
+	with open(DATABASE_FILENAME, 'rb') as file:
+		try:
+			file.seek(offset, 0)
+			value = file.read(size).decode(ENCODING)
+		except KeyError:
+			value = None
+
 	return value
 
 
--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 import unittest
 
 
-import loggy 
+import loggy
 
 
 class LoggyTest(unittest.TestCase):
     def test_set_and_get(self):
         loggy.set("42", "Douglas Adams")
         assert(loggy.get("42") == "Douglas Adams")
-    
+
     def test_set_updates_local_index(self):
-        key = 42
-        previous_index = loggy.index.get(42, None)
+        previous_index = loggy.index.get("42", None)
         loggy.set("42", "Test")
-        new_index = loggy.index.get(42, None)
+        new_index = loggy.index.get("42", None)
         assert(previous_index != new_index)
 
 
