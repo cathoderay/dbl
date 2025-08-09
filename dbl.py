9ec26ba025a2eb46bde0e61fc085872ad6e70c9c
Ronald Kaiser <raios.catodicos@gmail.com>
2025-08-09 20:37:48 -0300

    add command to compact file size

--- dbl.py
+++ dbl.py
@@ -10,7 +10,10 @@
 
 # Problem 2: the client looses it's index once the command is run.
 # Solution 1: have an index loaded in a instance always available.
-# Solution 2: Add repl to interact with the db. [chosen]
+# Solution 2: add repl to interact with the db. [chosen]
+
+# Problem 3: many lines for the same key can make the file size greater than it should be
+# Solution 1: offer an operation to compact it [chosen]
 
 
 import os
@@ -19,7 +22,8 @@ import sys
 from helper import print_debug, encode, decode
 
 
-DATABASE_FILENAME = "/tmp/db.loggy"
+DATABASE_FILENAME = "/tmp/dbl.data"
+COMPACT_TEMP_FILENAME = "/tmp/dbl.compact"
 END_RECORD = "\n"
 KEY_VALUE_SEPARATOR = ","
 DEBUG = False
@@ -35,7 +39,7 @@ def validate(key, value):
 	f"Value cannot contain new line character ({END_RECORD})"
 
 
-def set(key, value):
+def set(key, value, filename=DATABASE_FILENAME):
 	if DEBUG: print_debug("Inside set", key, value)
 
 	validate(key, value)
@@ -47,11 +51,12 @@ def set(key, value):
 
 	content = key_b + separator_b + value_b + end_b
 
-	END = os.path.getsize(DATABASE_FILENAME)
+	END = os.path.getsize(filename)
+
 	value_start = END + len(key_b) + len(separator_b)
 	index[key] = (value_start, len(value_b))
 
-	with open(DATABASE_FILENAME, 'ab+') as file:
+	with open(filename, "ab") as file:
 		file.seek(END, 0)
 		file.write(content)
 
@@ -59,7 +64,6 @@ def set(key, value):
 
 
 def build_index():
-	# TODO: check last key/value read and resume from there
 	if DEBUG: print_debug("Bulding index...")
 
 	with open(DATABASE_FILENAME, 'rb') as file:
@@ -104,6 +108,22 @@ def get(key):
 	return value
 
 
+def compact():
+	if DEBUG: print_debug("Inside compact")
+
+	if not index:
+		build_index()
+
+	file = open(COMPACT_TEMP_FILENAME, "w")
+	file.close()
+
+	for key in index:
+		value = get(key)
+		set(key, value, filename=COMPACT_TEMP_FILENAME)
+
+	print(f"Compact version generated at: {COMPACT_TEMP_FILENAME}")
+
+
 if __name__ == "__main__":
 	if "--debug" in sys.argv:
 		DEBUG = True
@@ -116,3 +136,7 @@ if __name__ == "__main__":
 		elif operator == "get":
 			value = get(*operands)
 			print(value)
+		elif operator == "compact":
+			compact()
+		elif operator == "build_index":
+			build_index()
