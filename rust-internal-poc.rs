3852bd86e0080155bf201a7012b62fe8de42bc60
Ronald Kaiser <raios.catodicos@gmail.com>
2025-08-22 20:32:34 -0300

    refactor function to build index (rust poc)

--- rust-internal-poc.rs
+++ rust-internal-poc.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
 use std::fmt;
+use std::io::SeekFrom;
+use std::io::Seek;
 
 
 impl fmt::Display for IndexValue {
@@ -12,39 +14,50 @@ impl fmt::Display for IndexValue {
 
 
 struct IndexValue {
-    start: usize,
-    size: usize,
+    start: u64,
+    size: u64,
 }
 
+
 const DATABASE_PATH: &str = "/tmp/dbl.data";
-const KEY_VALUE_SEPARATOR:char = ',';
+const KEY_VALUE_SEPARATOR: char = ',';
 
 
-fn build_index() -> HashMap<String, IndexValue> {
+fn build_index(bytes_read: u64) -> HashMap<String, IndexValue> {
     let mut file = match File::open(DATABASE_PATH) {
         Ok(f) => f,
         Err(e) => panic!("Failed to open file {}: {}", DATABASE_PATH, e),
     };
 
+    match file.seek(SeekFrom::Start(bytes_read)) {
+        Ok(f) => f,
+        Err(e) => panic!("Failed to seek file {}: {}", DATABASE_PATH, e),
+    };
+
     let mut content = String::new();
     match file.read_to_string(&mut content) {
-        Ok(_) => {
-            let mut index: HashMap<String, IndexValue> = HashMap::new();
-            for line in content.lines() {
-                let separator_index = line.find(KEY_VALUE_SEPARATOR).unwrap();
-                let value_size = line.len() - separator_index - 1;
-                let key = &line[0..separator_index];
-                index.insert(key.to_string(), IndexValue {start: separator_index, size: value_size});
-            }
-            return index;
-        },
+        Ok(f) => f,
         Err(e) => panic!("Failed to read file {}: {}", DATABASE_PATH, e),
+    };
+
+    let mut index: HashMap<String, IndexValue> = HashMap::new();
+    let mut current = bytes_read;
+    for line in content.lines() {
+        let separator_index = line.find(KEY_VALUE_SEPARATOR).unwrap();
+        let value_size: u64 = (line.len() - separator_index - 1) as u64;
+        let key: String = line[0..separator_index].to_string();
+        let value_start: u64 = current + (separator_index as u64) + 1;
+        index.insert(key.clone(), IndexValue {start: value_start, size: value_size});
+        current += (line.len() as u64) + 1;
     }
+    return index;
 }
 
 
 fn main() {
-    let index: HashMap<String, IndexValue> = build_index();
+    let index: HashMap<String, IndexValue>;
+    let bytes_read: u64 = 0u64;
+    index = build_index(bytes_read);
     for (key, value) in &index {
         println!("{key} => {value}");
     }
